a6975beac66c447e10bd4cee2c5dc142da3d0c81
Muhammed-Savad-pp <savad3517@gmail.com>
2025-07-01 22:14:35 +0530

    frontend url add

--- Server/src/index.ts
+++ Server/src/index.ts
@@ -27,7 +27,7 @@ app.use(morgan("combined", { stream: morganStream}));
 app.use(Express.json())
 
 app.use(cors({
-    origin:'http://localhost:5173',
+    origin: process.env.FRONT_END_URL,
     credentials:true
 }))
 
