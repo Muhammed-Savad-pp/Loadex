e8a36a92d53d88b167cd90d67e15e09009c04293
Muhammed-Savad-pp <savad3517@gmail.com>
2025-07-21 15:20:46 +0530

    fix admin side

--- Client/src/Routes/AdminRoutes.tsx
+++ Client/src/Routes/AdminRoutes.tsx
@@ -1,51 +1,45 @@
-import { Routes, Route } from 'react-router-dom'
-import TransporterProtectedRoute from '../ProtectedRoutes/TransporterProtectedRoute'
-import { Suspense, lazy } from 'react'
 
-const Profile = lazy(() => import('../pages/Transporter/Profile'))
-const RegisterTruck = lazy(() => import('../pages/Transporter/RegisterTruck'))
-const LoadBoard = lazy(() => import('../pages/Transporter/LoadBoard'))
-const MyTrucks = lazy(() => import('../pages/Transporter/MyTrucks'))
-const MyBids = lazy(() => import('../pages/Transporter/MyBids'))
-const Success = lazy(() => import('../pages/Transporter/Success'))
-const Failed = lazy(() => import('../pages/Transporter/Failed'))
-const Trips = lazy(() => import('../pages/Transporter/Trips'))
-const Directory = lazy(() => import('../pages/Transporter/Directory'))
-const Network = lazy(() => import('../pages/Transporter/Network'))
-const Subscription = lazy(() => import('../pages/Transporter/Subscription'))
-const SubscriptionSuccess = lazy(() => import('../pages/Transporter/SubscriptionSuccess'))
-const SubscriptionFailed = lazy(() => import('../pages/Transporter/SubscriptionFailed'))
-const PaymentHistory = lazy(() => import('../pages/Transporter/PaymentHistory'))
-const Chat = lazy(() => import('../pages/Transporter/Chat'))
-const Wallet = lazy(() => import('../pages/Transporter/Wallet'))
-const NotFound = lazy(() => import('../pages/common/NotFound'))
+import { Routes, Route } from "react-router-dom";
+import LoginPage from "../pages/admin/Login";
+import Dashboard from "../pages/admin/Dashboard";
+import AdminLoginProtector from "../ProtectedRoutes/AdminLoginProtector";
+import AdminProtectedRoute from "../ProtectedRoutes/AdminProtectedRoute";
+import Transporter from "../pages/admin/Transporter";
+import RequestTransporter from "../pages/admin/Request-Transporter";
+import Shipper from "../pages/admin/Shipper";
+import RequestShipper from "../pages/admin/Request-Shipper";
+import RequestTruck from "../pages/admin/Requested-Truck";
+import Load from "../pages/admin/Load";
+import NotFound from "../pages/common/NotFound";
+import Trips from "../pages/admin/Trips";
+import PaymentHistory from "../pages/admin/PaymentHistory";
 
-function TransporterRoute() {
-  return (
-    <Suspense fallback={<div>Loading...</div>}>
-      <Routes>
-        <Route element={<TransporterProtectedRoute />}>
-          <Route path='/profile' element={<Profile />} />
-          <Route path='/registerTruck' element={<RegisterTruck />} />
-          <Route path='/loadBoard' element={<LoadBoard />} />
-          <Route path='/myTrucks' element={<MyTrucks />} />
-          <Route path='/myBids' element={<MyBids />} />
-          <Route path='/success' element={<Success />} />
-          <Route path='/failed' element={<Failed />} />
-          <Route path='/trips' element={<Trips />} />
-          <Route path='/directory' element={<Directory />} />
-          <Route path='/network' element={<Network />} />
-          <Route path='/subscription' element={<Subscription />} />
-          <Route path='/subscription-success' element={<SubscriptionSuccess />} />
-          <Route path='/subscription-failed' element={<SubscriptionFailed />} />
-          <Route path='/paymentHistory' element={<PaymentHistory />} />
-          <Route path='/chat' element={<Chat />} />
-          <Route path='/wallet' element={<Wallet />} />
-          <Route path='*' element={<NotFound />} />
-        </Route>
-      </Routes>
-    </Suspense>
-  )
+
+const AdminRoutes = () => {
+    return (
+        
+        <Routes>
+            <Route element={<AdminLoginProtector/>}> 
+                <Route path="/login" element={<LoginPage/>} />
+            </Route>
+            
+            <Route element={< AdminProtectedRoute/>}>
+                <Route path="/dashboard" element={<Dashboard/>}/>
+                <Route path="/transporter" element={<Transporter/>}/>
+                <Route path="/shipper" element={<Shipper/>}/>
+                <Route path="/request/transporter" element={<RequestTransporter/>}/>
+                <Route path="/request/shipper" element={<RequestShipper/>}/>
+                <Route path="/request/truck" element={<RequestTruck />}/>
+                <Route path="/loads" element={<Load/>}/>
+                <Route path="/trips" element={<Trips/>}/>
+                <Route path="/paymentHistory" element={<PaymentHistory/>}/>
+                <Route path="*" element={<NotFound/>}/>
+                
+            </Route>
+        </Routes>
+
+
+    )
 }
 
-export default TransporterRoute
+export default AdminRoutes
